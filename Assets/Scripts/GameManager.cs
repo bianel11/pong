b4b712b255517c76a2479e79499dd1f5fb7e0da7
Bianel <bianel9815@gmail.com>
2022-06-30 01:18:16 -0400

    start/end audios and improves

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,56 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
+
+    AudioSource audioSource;
+    public AudioClip startGameSound, endGameSound;
+    private bool isStartScene = false;
+
+    // play audio when game starts
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Inicio")
+        {
+            isStartScene = true;
+
+            // Recuperar el componente audio source;
+            audioSource = GetComponent<AudioSource>();
+
+            audioSource.clip = startGameSound;
+            audioSource.Play();
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        // Si pulsa la tecla P o hace clic izquierdo empieza el juego
-        if (Input.GetKeyDown(KeyCode.P) || Input.GetMouseButtonDown(0))
+        if (isStartScene)
         {
-            // Cargo la escena de Juego 
-            SceneManager.LoadScene("Juego");
+            // Si pulsa la tecla P o hace clic izquierdo empieza el juego
+            if (Input.GetKeyDown(KeyCode.P) || Input.GetMouseButtonDown(0))
+            {
+                // Cargo la escena de Juego 
+                SceneManager.LoadScene("Juego");
+            }
         }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            audioSource = GetComponent<AudioSource>();
+
+            audioSource.clip = endGameSound;
+            audioSource.Play();
+
+            StartCoroutine(waitAudioPlays(audioSource));
+
+        }
+    }
+
+    IEnumerator waitAudioPlays(AudioSource audioSource)
+    {
+        // wait until the clip is done playing
+        yield return new WaitUntil(() => audioSource.isPlaying == false);
+        SceneManager.LoadScene("Inicio");
     }
 }
